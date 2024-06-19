71b6b6de0a2009510cfc416e3a049f67fbcec5c9
DylanoSol <dylanosolter@hotmail.com>
2024-06-19 20:58:22 +0200

    [FIXED] diffuse lighting might be correct now?

--- Shaders/VertexShader.hlsl
+++ Shaders/VertexShader.hlsl
@@ -32,36 +32,6 @@ struct VertexShaderOutput
     float4 Position : SV_Position;
 };
 
-// Function to calculate the inverse transpose of a float4x4 matrix
-float4x4 InverseTranspose(float4x4 mat)
-{
-    float4 c0 = mat[0];
-    float4 c1 = mat[1];
-    float4 c2 = mat[2];
-    float4 c3 = mat[3];
-
-    float4 r0 = float4(c0.x, c1.x, c2.x, 0.0);
-    float4 r1 = float4(c0.y, c1.y, c2.y, 0.0);
-    float4 r2 = float4(c0.z, c1.z, c2.z, 0.0);
-    float4 r3 = float4(0.0, 0.0, 0.0, 1.0);
-
-    float4x4 adjoint;
-    adjoint[0] = (cross(r1.xyz, r2.xyz), 1.0);
-    adjoint[1] = (-cross(r0.xyz, r2.xyz), 1.0);
-    adjoint[2] = (cross(r0.xyz, r1.xyz), 1.0);
-    adjoint[3] = float4(0.0, 0.0, 0.0, 1.0);
-
-    float det = dot(c0, adjoint[0]);
-
-    float4x4 result;
-    result[0] = adjoint[0] / det;
-    result[1] = adjoint[1] / det;
-    result[2] = adjoint[2] / det;
-    result[3] = r3;
-
-    return transpose(result);
-}
-
 VertexShaderOutput main(VertexPosColor IN)
 {
     VertexShaderOutput OUT;
@@ -71,17 +41,13 @@ VertexShaderOutput main(VertexPosColor IN)
     
     // Calculate normal matrix to transform the normal to world space
     float3x3 normalMatrix; 
-    normalMatrix[0] = Model.MODEL[0].xyz; 
-    normalMatrix[1] = Model.MODEL[1].xyz;
-    normalMatrix[2] = Model.MODEL[2].xyz;
     
-    normalMatrix = InverseTranspose(Model.MODEL); 
     
     OUT.Position = position;
     
     OUT.Color = float4(IN.Color, 1.0);
     
-    OUT.Normal = float4(mul(normalMatrix, IN.Normal), 1.0);
+    OUT.Normal = (IN.Normal, 1.0);
     
     OUT.LightDir = float4(normalize(LightPosCB.POS - worldPosition.xyz), 1.0);
 
